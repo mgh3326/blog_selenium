7e46db36af2a2891f25bf91a95789e6598554351
mgh3326 <mgh3326@naver.com>
2018-04-07 21:20:40 +0900

    update

---
+++ random_t.py
@@ -0,0 +1,6 @@
+from random import *
+
+
+print(randrange(1,7))  # 주사위처럼 1에서 6까지의 정수 중 하나를 무작위
+print(randrange(1,3))  # 주사위처럼 1에서 6까지의 정수 중 하나를 무작위
+print(randrange(1,6))  # 주사위처럼 1에서 6까지의 정수 중 하나를 무작위
