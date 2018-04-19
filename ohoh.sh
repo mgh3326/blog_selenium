9903e26e3ce540dcb9527673a737b4b7235b2769
mgh3326 <mgh3326@naver.com>
2018-04-20 08:14:21 +0900

    update

--- ohoh.sh
+++ ohoh.sh
@@ -1,1 +1,10 @@
-while [ true ]; do python blog_selenium.py; sleep 200; done
+platform='unknown'
+unamestr=`uname`
+if [ "$unamestr" == 'Linux' ]; then
+	   platform='linux'
+fi
+if [[ $platform == 'linux' ]]; then
+	while [ true ]; do python3 blog_selenium.py; sleep  $(( ( RANDOM % 100 )  + 60 )); done
+else
+	while [ true ]; do python blog_selenium.py; sleep  $(( ( RANDOM % 100 )  + 60 )); done
+fi
