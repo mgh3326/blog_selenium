cd27544c4a7fa52b36a00cb8ccff8875ab3e42cf
mgh3326 <mgh3326@naver.com>
2018-04-20 08:17:12 +0900

    update

--- ohoh.sh
+++ ohoh.sh
@@ -1,10 +1,9 @@
 platform='unknown'
 unamestr=`uname`
 if [ "$unamestr" == 'Linux' ]; then
-	   platform='linux'
-fi
-if [[ $platform == 'linux' ]]; then
+	killall chromium-browser
 	while [ true ]; do python3 blog_selenium.py; sleep  $(( ( RANDOM % 100 )  + 60 )); done
 else
 	while [ true ]; do python blog_selenium.py; sleep  $(( ( RANDOM % 100 )  + 60 )); done
 fi
+
